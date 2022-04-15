851d7f52b3896b61d5c2c2e58616e77ea9246f4a
coryla <cory.lance.anderson@gmail.com>
2022-04-15 13:58:11 -0500

    destroy  wokring, working on api tests

--- api/index.js
+++ api/index.js
@@ -2,22 +2,18 @@
 // attach other routers from files in this api directory (users, activities...)
 // export the api router
 const express = require("express");
-const cors = require("cors");
 const bcrypt = require("bcrypt");
 const { createUser } = require("../db/users");
-const activitiesRouter = require("./activities");
 const apiRouter = express.Router();
-apiRouter.use(cors());
-apiRouter.post("/users/register", async (req, res, next) => {
-  try {
-    const username = req.params.username;
-    const password = req.params.password;
-    const user = await createUser({ username, password });
-    console.log(user, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-    res.send(user);
-  } catch (error) {
-    next(error);
-  }
+
+apiRouter.get("/health", (req, res, next) => {
+  res.send({ message: "Server is healthy all is well" });
 });
+
+const usersRouter = require("./users");
+apiRouter.use("/users", usersRouter);
+
+const activitiesRouter = require("./activities");
+
 apiRouter.use("/activities", activitiesRouter);
 module.exports = apiRouter;
--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 // create the express server here
 const express = require("express");
 const morgan = require("morgan");
-const apiRouter=require("./api");
-const {client}=require("./db");
-const {PORT = 3000}=process.env;
-const server=express();
+const apiRouter = require("./api");
+const { client } = require("./db");
+const { PORT = 3000 } = process.env;
+const server = express();
+const cors = require("cors");
 
 server.use(express.json());
 server.use(morgan("dev"));
+server.use(cors());
 
 client.connect();
-server.use("/api",apiRouter);
-server.listen(PORT, ()=>{
-    console.log("The server is up on post", PORT);
+server.use("/api", apiRouter);
+server.listen(PORT, () => {
+  console.log("The server is up on post", PORT);
 });
